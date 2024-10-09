7584607f8a06e918d0ca655a1a61f5f73c49c92f
yun <ly.yun811@gmail.com>
2024-10-09 13:30:02 -0700

    add useEffect to see different effects and page rendering

--- src/App.js
+++ src/App.js
@@ -3,21 +3,46 @@ import './App.css';
 
 function App() {
   const [page, setPage] = useState(1);
-  const [option, setOption] = useState('0');
+  const [option, setOption] = useState('React');
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    console.log("current page", page);
+      fetch('https://api.github.com/repos/facebook/' + option +'/issues?page=' + page)
+      // fetch('https://api.github.com/repos/facebook/react/issues')
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log({data});
+          setIssues(data)
+        })
+      // console.log("page or option changed", page, option)
+  }, [])
+
+  useEffect(() => {
+    console.log("current option", option);
+  }, [option])
+
   return (
     <div className="App">
       <h2> Page {page} </h2>
       <div>
         <button onClick= { () => setPage(Math.max(1, page -1))}> Previous </button>
-        <button onClick= { () => setPage(Math.max(1, page +1))}> Next </button>
+        <button onClick= { () => setPage(Math.max(6, page +1))}> Next </button>
       </div>
 
       <h3> Selected:{option} </h3>
       <select onChange={(eve) => setOption(eve.target.value)}>
-        <option value="most popular">most popular</option>
-        <option value="most viewed">most viewed</option>
-        <option value="recent added">recent added</option>
+        <option value="react">React</option>
+        <option value="create-react-app">Create React App</option>
       </select>
+
+      <ul>
+        {
+          issues.map(issue => {
+            return (<li key={issues.title}> {issue.title} </li>)
+          })
+        }
+      </ul>
 
     </div>
   );
